4669b3ea37c5015fa8917ae52d6587db9124548c
Juanjito G <129878536+gomsago@users.noreply.github.com>
2025-02-17 13:44:58 -0500

    tallerJS
    Funciones de JS resueltas

--- solutions.js
+++ solutions.js
@@ -1,26 +1,45 @@
 // Sum of Two Numbers
 function sum(a, b) {
-  // Add your code here
+  return a + b; 
 }
 
 // Factorial of a Number
 function factorial(n) {
-  // Add your code here
+  if(n==0){
+    return 1;
+  }
+  let result = 1; 
+  for(i=1; i<=n; i++){
+    result *= i; 
+  }
+  return result;
 }
 
 // Find the Largest Number
 function findLargest(arr) {
-  // Add your code here
+  return Math.max(...arr);
 }
 
 // Count Vowels in a String
 function countVowels(str) {
-  // Add your code here
+  let count =0;
+  const vowels = 'aeiouAEIOU'
+  if(vowels.includes(str[i])){
+    count++;
+  }
+  return count;
 }
 
 // Check if a Number is Prime
 function isPrime(n) {
-  // Add your code here
+  if(n<=1){
+    return false;
+  }
+  for( let i =2; i <= Math.sqrt(n); i++){
+    if(n% i === 0){
+      return false;
+    }
+  }
 }
 
 module.exports = {
